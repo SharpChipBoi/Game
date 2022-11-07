0d6afab941032bd6dcebd06c9b2f3a77c9e69fec
Sophia <petyaevas@gmail.com>
2022-11-07 07:09:55 +0300

    Working achievements

--- Dreamscape/Assets/Scripts/Achievements/Achievement.cs
+++ Dreamscape/Assets/Scripts/Achievements/Achievement.cs
@@ -1,19 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Achievement
 {
 
     private string name;
 
+    public string Name //позволяет получить доступ из другого скрипта
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
     private string description;
+
+    public string Description 
+    {
+        get { return description; }
+        set { description = value; }
+    }
 
     private bool unlocked;
 
+    public bool Unlocked
+    {
+        get { return unlocked; }
+        set { unlocked = value; }
+    }
+
     private int spriteIndex;
 
+    public int SpriteIndex
+    {
+        get { return spriteIndex; }
+        set { spriteIndex = value; }
+    }
+
+
     private GameObject achievementRef;
+
+    private List<Achievement> dependencies = new List<Achievement>();
+
+    private string child;
+
+    public string Child
+    {
+        get { return child; }
+        set { child = value; }
+    }
 
     public Achievement(string name, string description, int spriteIndex, GameObject achievementRef)
     {
@@ -22,17 +58,49 @@ public class Achievement
         this.unlocked = false;
         this.spriteIndex = spriteIndex;
         this.achievementRef = achievementRef;
+        LoadAchievement();
     }
 
 
+    public void AddDependency(Achievement dependency)
+    {
+        dependencies.Add(dependency);
+    }
+
     public bool EarnAchievement() //проверка на получение ачивки
     {
-        if (!unlocked)
+        if (!unlocked && !dependencies.Exists(x => x.unlocked == false)) //проверка если хотя бы одна ачивка не открыта, то false
         {
-            unlocked = true;
+            achievementRef.GetComponent<Image>().sprite = AchievementManager.Instance.unlockedSprite;
+
+            SaveAchievement(true);
+
+            if (child != null)
+            {
+                AchievementManager.Instance.EarnAchievement(child); // проверка есть ли листья (children) у ачивок
+            }
+
             return true;
         }
         return false;
+    }
+
+    public void SaveAchievement(bool value) // сохраняем данные об открытых ачивках
+    {
+        unlocked = value;
+
+        PlayerPrefs.SetInt(name, value ? 1 : 0); // проверка на полученную ачивку (если загрузим 1, то открыли ачивку, если 0 то нет)
+
+        PlayerPrefs.Save();
+    }
+
+    public void LoadAchievement()
+    {
+        unlocked = PlayerPrefs.GetInt(name) == 1 ? true : false;// проверка на полученную ачивку (если загрузим 1, то открыли ачивку, если 0 то нет)
+        if (unlocked)
+        {
+            achievementRef.GetComponent<Image>().sprite = AchievementManager.Instance.unlockedSprite;
+        }
     }
 
 }
--- Dreamscape/Assets/Scripts/Achievements/AchievementManager.cs
+++ Dreamscape/Assets/Scripts/Achievements/AchievementManager.cs
@@ -20,13 +20,36 @@ public class AchievementManager : MonoBehaviour
 
     public Dictionary<string, Achievement> achievements = new Dictionary<string, Achievement>();
 
+    public Sprite unlockedSprite;
+
+    private static AchievementManager instance; // создаем сингалтон чтобы получить доступ к классу и к его дате
+
+    public static AchievementManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<AchievementManager>(); //находит ачивмент менеджер и  пихает его в instance чьлбы мы могли использовать его класс
+            }
+            return AchievementManager.instance; 
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-
+        PlayerPrefs.DeleteAll(); // очистить дату от сохраненной 
         activeButton = GameObject.Find("GeneralBtn").GetComponent<AchievementButton>();
         CreateAchievement("General", "Press W", "Press W to unlock this achievement", 0);
+        CreateAchievement("General", "Press A", "Press A to unlock this achievement", 0);
+        CreateAchievement("General", "Press S", "Press S to unlock this achievement", 0);
+        CreateAchievement("General", "Press D", "Press D to unlock this achievement", 0);
+        CreateAchievement("General", "All keys", "Press all keys to unlock", 1, new string[] { "Press W" ,"Press A", "Press S", "Press D"});
 
+
+        CreateAchievement("Other", "Dash", "Find out how to dash", 2);
+        CreateAchievement("Other", "Jump", "Find out how to jump", 3);
         foreach (GameObject achievementList in GameObject.FindGameObjectsWithTag("AchievementList")) //смотрим у кого есть тэг ачивментлист и отключаем, чтобы не было overlapping
         {
             achievementList.SetActive(false);
@@ -49,13 +72,35 @@ public class AchievementManager : MonoBehaviour
         {
             EarnAchievement("Press W");
         }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            EarnAchievement("Press S");
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            EarnAchievement("Press A");
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            EarnAchievement("Press D");
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            EarnAchievement("Dash");
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            EarnAchievement("Jump");
+        }
     }
 
-    public void EarnAchievement(string title) //Получаем ачивку
+    public void EarnAchievement(string title) //Получаем ачивку и показываем ее на экране
     {
         if (achievements[title].EarnAchievement())
         {
             GameObject achievement = (GameObject)Instantiate(visualAchievement);
+            SetAchievementInfo("EarnCanvas", achievement, title);
+            achievement.transform.localScale = new Vector3(5, 1, 1);
             StartCoroutine(HideAchievement(achievement)); 
         }
     }
@@ -66,7 +111,7 @@ public class AchievementManager : MonoBehaviour
         Destroy(achievement);
     }
 
-    public void CreateAchievement(string category, string title, string description, int spriteIndex)//создаем ачивку
+    public void CreateAchievement(string parent, string title, string description, int spriteIndex, string[] dependencies = null)//создаем ачивку. Если мв не хоти создавать ачивку, которая зависит от выполнения других, то просто ничего не пишеи при использовании ф-ции 
     {
         GameObject achievement = (GameObject)Instantiate(achievementPrefab);
 
@@ -74,17 +119,29 @@ public class AchievementManager : MonoBehaviour
 
         achievements.Add(title, newAchievement);
 
-        
-        SetAchievementInfo(category, achievement, title, description, spriteIndex);
+        SetAchievementInfo(parent, achievement, title);
+
+        if (dependencies != null)
+        {
+            foreach(string achievementTitle in dependencies) //ищем правильное название ачивки и создаем зависимость 
+            {
+                Achievement dependency = achievements[achievementTitle];
+                dependency.Child = title;
+                newAchievement.AddDependency(dependency);
+            }
+            //создали связь между ачивками dependancy = "Press Space" <-- Child = "Press W"
+            //NewAchievement = "Press W" --> Parent = "Press Space" 
+        }
+
     }
 
-    public void SetAchievementInfo(string category, GameObject achievement, string title, string description, int spriteIndex)//выдает всю информацию об ачивке (размер,какое изображение использовать, чей ребонок и тд.)
+    public void SetAchievementInfo(string parent, GameObject achievement, string title)//выдает всю информацию об ачивке из словаря achievements(размер,какое изображение использовать, чей ребонок и тд.)
     {
-        achievement.transform.SetParent(GameObject.Find(category).transform);
+        achievement.transform.SetParent(GameObject.Find(parent).transform);
         achievement.transform.localScale = new Vector3(1, 1, 1);
         achievement.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = title;
-        achievement.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = description;
-        achievement.transform.GetChild(2).GetComponent<Image>().sprite = sprites[spriteIndex];
+        achievement.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = achievements[title].Description;
+        achievement.transform.GetChild(2).GetComponent<Image>().sprite = sprites[achievements[title].SpriteIndex];
     }
 
     public void ChangeCategory(GameObject button) //меняем активированное поле(категорию)
